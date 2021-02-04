f0ae20a775fb92f0334fd829afaa8fd7055edfc2
Kelli Scheuble <github@scheuble.dev>
2021-02-04 13:49:56 -0800

    Creates more scaffolding for the Grid, Cell, and Experiment classes

--- src/classes.py
+++ src/classes.py
@@ -8,10 +8,10 @@ class Grid:
         self.cells_occupied = 0
         for row in self.grid:
             for cell in row:
-                if cell.livable:
+                if cell.status == "occupied":
                     self.cells_occupied += 1
     
-    def initialize_grid(self, file_name):
+    def initialize_grid(self, file_name:str):
         self.grid = list() # Make sure empty grid
         with open(file_name) as f:
             for line in f:
@@ -19,17 +19,46 @@ class Grid:
                 self.grid.append([Cell(character) for character in line[:-1]])
 
     def apply_rules(self):
+        """
+        Returns another grid for the next hour's growth
+        """
         
+        def count_neighbors(i, j):
+            neighbors = 0
+            # Make sure this is looking at edge cases (literal edges - should they be counted as unlivable??)
+            # Make this more reusable and not so repetitive 
+            if self.grid[[i+1][j]].status == "occupied":
+                neighbors += 1
+
+
+        def determine_if_livable(cell, i, j):
+            if cell.livable & count_neighbors(i, j) < 4:
+                return True
+            return False
+            
+        next_hour_grid = list()
+
+        # iterate through the grid and return a new one.
+        for i, row in enumerate(self.grid):
+            current_row = list()
+            for j, cell in enumerate(row):
+                if determine_if_livable(cell, i, j):
+                    current_row.append(["X"])
+                else:
+                    current_row.append(["."])
+            next_hour_grid.append(current_row)
 
     
 class Cell:
 
     def __init__(self, character):
+
+        CHARACTER_MAP = {"X": "occupied", 
+                         "L":"livable", 
+                         ".": "unlivable"}
+
         self.character = character
-        if character == "L" or character == "#":
-            self.livable = True
-        else:
-            self.livable = False
+        self.status = CHARACTER_MAP[self.character]
 
     def __repr__(self):
         return f"{self.character}"
@@ -39,14 +68,13 @@ class Experiment:
 
     def __init__(self, initial_grid):
         self.initial_grid = initial_grid
-        # what's best practice to do with this when
-        # we don't necessarily need it when we initialize it?
-        self.previous_grid = list()
-        self.current_grid = list()
         self.is_stable = False
         self.hour = 0
 
     def run_experiment(self):
+
+        previous_grid = self.initial_grid
+        current_grid = list()
 
         while not self.is_stable:
             self.hour += 1
